e077a565aa97cd327db683efee6284b3ee923e63
ShipaShipovnik <shipovnik2020d@gmail.com>
2024-09-11 15:32:50 +0300

    Initial commit

---
+++ avito/api/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.db.models import CharField, ForeignKey
+
+
+class Post(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    price = models.FloatField(blank=False)
+    body = models.TextField(blank=True, default='')
+    owner = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE)
+    categories = models.ManyToManyField('Category', related_name='post_set', blank=True)
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    body = models.TextField(blank=False)
+    stars = models.IntegerField(blank=False)
+    owner = models.ForeignKey('auth.User', related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', related_name='comments', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return self.body
+
+
+class Category(models.Model):
+    name = CharField(max_length=100, blank=False, default='')
+    owner = models.ForeignKey('auth.User', related_name='categories', on_delete=models.CASCADE)
+    posts = models.ManyToManyField('Post', related_name='category_set', blank=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
